5a29eb92700f0d56d9585721fd0f245bb125b033
abdullahgh8900 <atg789000@gmail.com>
2025-11-06 19:55:01 +0200

    Member Controller - Views & Update Member Service

--- GymManagementBLL/BusinessServices/implementaion/MemberService.cs
+++ GymManagementBLL/BusinessServices/implementaion/MemberService.cs
@@ -128,17 +128,17 @@ namespace GymManagementBLL.BusinessServices.implementaion
             #endregion
 
             var MemberViewModel = _mapper.Map<MemberViewModel>(member);
-            var memership = _unitOfWork.GetRepository<Membership>()
-                            .GetAll(M => M.MemberId == member.Id && M.Status == "Active")
-                            .FirstOrDefault();
-            if (memership is null) return null;
+            var ActiveMembership = _unitOfWork.GetRepository<Membership>()
+                                              .GetAll(m => m.MemberId == memberId && m.Status == "Active").FirstOrDefault();
 
-            MemberViewModel.MembershipStartDate = memership.CreatedAt.ToShortDateString();
-            MemberViewModel.MembershipEndDate = memership.EndDate.ToShortDateString();
-            var plan = _unitOfWork.GetRepository<Plan>().GetById(memership.PlanId);
-            if (plan is null) return null;
-
-            MemberViewModel.PlanName = plan.Name;
+            if (ActiveMembership is not null)
+            {
+                var activePlan = _unitOfWork.GetRepository<Plan>().GetById(ActiveMembership.PlanId);
+                if (activePlan is null) return null;
+                MemberViewModel.PlanName = activePlan?.Name;
+                MemberViewModel.MembershipEndDate = ActiveMembership.EndDate.ToShortDateString();
+                MemberViewModel.MembershipStartDate = ActiveMembership.CreatedAt.ToShortDateString();
+            }
 
             return MemberViewModel;
 
@@ -190,8 +190,8 @@ namespace GymManagementBLL.BusinessServices.implementaion
 
             try
             {
-                if (IsEmailExit(memberToUpdate.Email) || IsPhoneExit(memberToUpdate.Phone))
-                    return false;
+                var emailExist = _unitOfWork.GetRepository<Member>().GetAll(x => x.Email == memberToUpdate.Email && x.Id != memberId);
+                var phoneExist = _unitOfWork.GetRepository<Member>().GetAll(x => x.Phone == memberToUpdate.Phone && x.Id != memberId);
 
                 var member = _unitOfWork.GetRepository<Member>().GetById(memberId);
                 if (member is null) return false;
