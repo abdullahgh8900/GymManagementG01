740c320ca301e5f516f67acbb7969266c6e80bdf
abdullahgh8900 <atg789000@gmail.com>
2025-11-20 15:26:23 +0200

    Add Account Service and View Model

---
+++ GymManagementBLL/BusinessServices/implementaion/AccountService.cs
@@ -0,0 +1,34 @@
+﻿using GymManagementBLL.Services.Interfaces;
+using GymManagementBLL.ViewModels.AccountViewModels;
+using GymManagementDAL.Entities;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementBLL.Services.Classes
+{
+    public class AccountService : IAccountService
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AccountService(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public ApplicationUser? ValidateUser(LoginViewModel loginViewModel)
+        {
+            var user = _userManager.FindByEmailAsync(loginViewModel.Email).Result;
+
+            if (user == null) return null;
+
+            var isPasswordValid = _userManager.CheckPasswordAsync(user, loginViewModel.Password).Result;
+
+            return isPasswordValid ? user : null;
+
+        }
+    }
+}
